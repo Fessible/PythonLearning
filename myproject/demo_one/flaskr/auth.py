3f1fb6f7d5629ef8fc06bace2a066d2944287c83
rhm <499953902@qq.com>
2019-07-04 18:02:15 +0800

    update

--- myproject/demo_one/flaskr/auth.py
+++ myproject/demo_one/flaskr/auth.py
@@ -22,12 +22,17 @@ def register():
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-        elif db.execute('SELECT id FROM user WHERE username =?', (username)).fetchone() is not None:
+        elif db.execute('SELECT id FROM user WHERE username =?', (username,)).fetchone() is not None:
             error = 'User {} is already registered.'.format(username)
 
         if error is None:
-            db.execute('INSERT INTO user (username,password) VALUES (?,?)'), (
-                username, generate_password_hash(password))
+            db.execute('INSERT INTO user (username,password) VALUES (?,?)', (
+                username, generate_password_hash(password)))
+#
+#                db.execute(
+#                           'INSERT INTO user (username, password) VALUES (?, ?)',
+#                           (username, generate_password_hash(password))
+#                           )
             db.commit()
             return redirect(url_for('auth.login'))
         flash(error)
