c810f2b1a670d6dd8016e855a16f45e16fd8d95b
rhm <499953902@qq.com>
2019-07-16 15:38:27 +0800

    鱼书基础配置

---
+++ myproject/fisher/app/web/book.py
@@ -0,0 +1,28 @@
+from flask import jsonify, request
+
+from yushu_book import YuShuBook
+from helper import is_isbn_or_key
+# 这里导入的app，执行if __name__时==fisher，所以没有执行run
+# from fisher import app
+
+from . import web
+from app.web.forms.book import SearchForm
+
+
+# http://127.0.0.1:5000/book/search/9787501524044/1
+@web.route('/book/search')
+def search():
+    form = SearchForm(request.args)
+    if form.validate():
+        q = form.q.data.strip()
+        page = form.page.data
+        is_isbn_or_key(q)
+        isb_or_key = 'isbn'
+        if isb_or_key == 'isbn':
+            result = YuShuBook.search_by_isbn(q)
+        else:
+            result = YuShuBook.search_by_keyword(q, page=page)
+        # dict 序列化
+        # return json.dumps(result), 200, {'content-type': 'application/json'}
+        return jsonify(result)
+    return jsonify(form.errors)
--- myproject/fisher/fisher.py
+++ myproject/fisher/fisher.py
@@ -1,16 +1,6 @@
-from flask import Flask, make_response
-from helper import is_isbn_or_key
+from app import create_app
 
-app = Flask(__name__)
-# import config
-# 也可以使用from_object(config)
-app.config.from_object('config')
-
-
-@app.route('/book/search/<q>/<page>')
-def search(q, page):
-    is_isbn_or_key(q)
-
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=app.config['DEBUG'], host='0.0.0.0')
