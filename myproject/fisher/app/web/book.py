6311f4617ff78c2933ac73f66a2abfab86656131
rhm <499953902@qq.com>
2019-07-17 17:52:24 +0800

    添加资源

--- myproject/fisher/app/web/book.py
+++ myproject/fisher/app/web/book.py
@@ -1,53 +1,77 @@
+"""
+ Created by 七月 on 2018-2-1.
+"""
+from flask import jsonify, request, current_app, url_for, render_template, flash
+from app.forms.book import SearchForm
 import json
 
-from flask import jsonify, request, render_template
-
-from app.spider.yushu_book import YuShuBook
 from app.libs.helper import is_isbn_or_key
-# 这里导入的app，执行if __name__时==fisher，所以没有执行run
-# from fisher import app
-
+from app.spider.yushu_book import YuShuBook
+from app.view_models.book import BookViewModel, BookCollection
 from . import web
-from app.web.forms.book import SearchForm
-from app.view_models.book import BookCollection
+
+__author__ = '七月'
 
 
-# http://127.0.0.1:5000/book/search/9787501524044/1
 @web.route('/book/search')
 def search():
+    """
+        q :普通关键字 isbn
+        page
+        ?q=金庸&page=1
+    """
+
     form = SearchForm(request.args)
     books = BookCollection()
 
     if form.validate():
         q = form.q.data.strip()
         page = form.page.data
-        is_isbn_or_key(q)
+        isbn_or_key = is_isbn_or_key(q)
         yushu_book = YuShuBook()
 
-        isb_or_key = 'isbn'
-        if isb_or_key == 'isbn':
+        if isbn_or_key == 'isbn':
             yushu_book.search_by_isbn(q)
-
         else:
-
             yushu_book.search_by_keyword(q, page)
-            # 原来返回的数据是 book:{'total':1,}的json数据格式
-            # result = YuShuBook.search_by_keyword(q, page=page)
-            # result = BookViewModel.package_collection(result, q)
 
-        # dict 序列化
-        # return json.dumps(result), 200, {'content-type': 'application/json'}
-        # return jsonify(result)
         books.fill(yushu_book, q)
-        # books是一个对象，所以要讲books转化成dict
-        return json.dumps(books, default=lambda o: o.__dict__)
-    return jsonify(form.errors)
+    else:
+        flash('搜索的关键字不符合要求，请重新输入关键字')
+        # return jsonify(form.errors)
+    return render_template('search_result.html', books=books)
+
+
+@web.route('/book/<isbn>/detail')
+def book_detail(isbn):
+    pass
 
 
 @web.route('/test')
 def test():
-    data = {
-        'title': None
+    r = {
+        'name': None,
+        'age': 18
     }
+    # data['age']
+    r1 = {
 
-    return render_template('test.html', data=data)
+    }
+    flash('hello,qiyue', category='error')
+    flash('hello, jiuyue', category='warning')
+    # 模板 html
+    return render_template('test.html', data=r, data1=r1)
+
+
+@web.route('/test1')
+def test1():
+    print(id(current_app))
+    from flask import request
+    from app.libs.none_local import n
+    print(n.v)
+    n.v = 2
+    print('-----------------')
+    print(getattr(request, 'v', None))
+    setattr(request, 'v', 2)
+    print('-----------------')
+    return ''
